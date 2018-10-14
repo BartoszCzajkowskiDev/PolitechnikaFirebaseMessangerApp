cb8e214ff2884576119d4c449be03f078e9af943
bartoszczajkowski <bpczajkowski@gmail.com>
2018-10-14 12:24:58 +0200

    Login panel added

---
+++ app/src/main/java/com/example/bartoszczajkowski/politechnikafirebasemessangerapp/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bartoszczajkowski.politechnikafirebasemessangerapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.os.ResultReceiver
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/bartoszczajkowski/politechnikafirebasemessangerapp/MainActivity.kt
+++ app/src/main/java/com/example/bartoszczajkowski/politechnikafirebasemessangerapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.bartoszczajkowski.politechnikafirebasemessangerapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity", "Email is calling: " + email)
+            Log.d("MainActivity", "Password: $password")
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
